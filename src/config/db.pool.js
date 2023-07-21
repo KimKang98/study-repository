153c75b6304bbdb0d94889acb3dbe3149897959d
Kang Kim <kimkang98@gmail.com>
2023-07-21 10:52:10 +0900

    second comit

--- src/config/db.pool.js
+++ src/config/db.pool.js
@@ -9,6 +9,6 @@ const pool = new sql.ConnectionPool(config)
   })
   .catch((err) => console.log("Database Connection Failed! Bad Config: ", err));
 
-console.log("test");
+console.log("test2");
 
 module.exports = pool;
